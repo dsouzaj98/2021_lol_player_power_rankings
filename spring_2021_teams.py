1d643c5ebca05507942718efc83669378791974c
dsouzaj98 <66758205+dsouzaj98@users.noreply.github.com>
2021-12-20 13:34:09 -0500

    adding team ranking table

---
+++ spring_2021_teams.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+res=pd.read_excel('LCS_team_predictions.xlsx')
+
+new_cols=dict(keys=res.columns)
+res.columns = res.columns.str.replace("Rank the team's performance in spring split", "")
+
+
+df=res.copy()
+df.drop(columns='Timestamp', axis=1, inplace=True)
+
+df.set_index(['Name'], inplace=True)
+
+df.loc['Average Rank']=df.mean()
+df=df.round(2)
+df=df.T
+df.sort_values('Average Rank', axis=0, inplace=True)
+print(df.to_markdown())
